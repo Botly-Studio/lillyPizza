2c5b8d46a392c34f8f4f23beb741efff2a33ca99
Jules Topart <jules.topart@gmail.com>
2019-08-23 15:48:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const config = {
   port: 3000,
   log : false,
-  //origin : 'http://beta.botly-studio.fr',
-  origin : 'http://localhost:8080',
+  origin : 'http://beta.botly-studio.fr',
+  //origin : 'http://localhost:8080',
   compileScriptPath : "/opt/compiler/compile.sh"
 };
 
