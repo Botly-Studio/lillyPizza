91d089da67a8cf8a9fe53b40a0bafea5199017b7
JulesTopart <jules.topart@gmail.com>
2020-07-02 16:52:47 +0200

    fix path

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const config = {
 		'http://botly-studio.fr',
         'http://localhost:8080'
     ],
-    compileScriptPath: "/opt/lillyPizza/compile.sh",
-    compiledSketchPath: "/opt/lillyPizza/sketch/build/arduino.avr.LilyPadUSB/sketch.ino.hex"
+    compileScriptPath: "/opt/lillypizza/compile.sh",
+    compiledSketchPath: "/opt/lillypizza/sketch/build/arduino.avr.LilyPadUSB/sketch.ino.hex"
 };
 
 module.exports = config;
